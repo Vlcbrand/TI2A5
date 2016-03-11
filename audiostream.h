4e8d6f4b10ae7a382601911d537c372da6b1bc56
Gilian Joosen <gilian_joosen@hotmail.com>
2016-03-11 13:19:54 +0100

    gilian is homo

--- audiostream.h
+++ audiostream.h
@@ -1,10 +1,25 @@
 //
 // Created by Gilian Joosen on 10/03/16.
 //
-
 #ifndef MULTI_OS_BUILD_AUDIOSTREAM_H
 #define MULTI_OS_BUILD_AUDIOSTREAM_H
 
-int play_stream(void);
+int STOP_THREAD;
+
+typedef struct radio_stream {
+    char *name; //name
+    char *radio_ip; //ip
+    char *radio_url; //url
+    int radio_port;
+} RADIO_STREAM;
+
+RADIO_STREAM *yorick;
+RADIO_STREAM *radio_3fm;
+
+initAudioStreams();
+
+int play_stream(RADIO_STREAM rStream);
+
+THREAD(PlayStream, args);
 
 #endif //MULTI_OS_BUILD_AUDIOSTREAM_H
